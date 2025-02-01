0eb2315f1246744d1a85b1ca6556864cf7eb3f84
zlyons70 <zlyons70@gmail.com>
2025-01-31 21:00:21 -0500

    Created game initialize fn and game to string, started FEN initialization

--- Chess/src/main.rs
+++ Chess/src/main.rs
@@ -1,3 +1,12 @@
+use constants::{COL_MAP, MODTABLE, PiecePosition};
+mod constants;
+use game::Game;
+mod game;
+pub mod board;
+pub mod pieces;
+
+
 fn main() {
-    println!("Hello, world!");
+    let new_game = Game::initialize();
+    println!("{}", new_game.to_string())
 }
