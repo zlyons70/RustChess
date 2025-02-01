4a1f8f74714e8c6459ac996ad3cca2a6f6926afe
zlyons70 <zlyons70@gmail.com>
2025-02-01 00:01:02 -0500

    Finished fen initialization for the board

--- Chess/src/game.rs
+++ Chess/src/game.rs
@@ -5,10 +5,11 @@ use crate::constants::*;
 pub struct Game{
     pieces: Vec<Piece>,
     squares: Vec<Square>,
-    active_team: char,
+    active_team: Color,
     winner: bool,
     winner_team: String,
     fen: String,
+    en_passant: Option<PiecePosition>,
     w_king_castle: bool,
     b_king_castle: bool,
     w_queen_castle: bool,
@@ -17,9 +18,7 @@ pub struct Game{
     full_move: usize,
 }
 
-// the goal of the to fn string is to take a board, and convert the board into a string
-// and by board we me each square
-// for each square we keep track of the index of the square and what piece it is
+
 impl Game {
     fn push_square_and_piece(&mut self, piece_char: &char, index: &mut usize) {
         if piece_char.is_alphabetic() {
@@ -30,7 +29,7 @@ impl Game {
                 'b' => PieceType::Bishop,
                 'n' => PieceType::Knight,
                 'r' => PieceType::Rook,
-                _ => PieceType::Pawn
+                _ => panic!("Invalid Piece")
             };
             let mut cur_color = Color::Black;
             if piece_char.is_uppercase() {
@@ -54,19 +53,25 @@ impl Game {
 
     pub fn initialize() -> Game {
         // Creates initail chess board state
+       Game::fen_initialize("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1".to_string())
+    }
+
+    pub fn fen_initialize(fen: String) -> Game {
+        // This function takes a FEN string and creates a game struct
         let mut game = Game {
             pieces: vec![],
             squares: vec![],
-            active_team: 'w',
+            active_team: Color::White,
             winner: false,
             winner_team: "None".to_string(),
-            fen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1".to_string(),
-            w_king_castle: true,
-            b_king_castle: true,
-            w_queen_castle: true,
-            b_queen_castle: true,
+            fen: fen.clone(),
+            w_king_castle: false,
+            b_king_castle: false,
+            w_queen_castle: false,
+            b_queen_castle: false,
             half_move: 0,
             full_move: 0,
+            en_passant: None,
 
         };
         let mut actual_index: usize = 0;
@@ -78,7 +83,64 @@ impl Game {
                 game.push_square_and_piece(&c, &mut actual_index);
             }
         }
+
+        // once the game board is initialized we can use the split on function to 
+        // look at the game state chars
+        let (_, rest) = Game::split_on(&game.fen, ' ');
+        let rest = rest.to_string();
+        let (color, rest) = Game::split_on(&rest, ' ');
+        match color {
+            "w" => game.active_team = Color::White,
+            "b" => game.active_team = Color::Black,
+            _ => panic!("Non color")
+        }
+        // Do castleing rights
+        let rest = rest.to_string();
+        let (castle_rights, rest) = Game::split_on(&rest, ' ');
+        for c in castle_rights.chars() {
+            match c {
+                'K' => game.w_king_castle = true,
+                'Q' => game.w_queen_castle = true,
+                'k' => game.b_king_castle = true,
+                'q' => game.b_queen_castle = true,
+                '-' => (),
+                _ => panic!("castle rights error")
+            }
+        }
+        // do en_passant
+        let rest = rest.to_string();
+        let (enpass, rest) = Game::split_on(&rest, ' ');
+        match enpass {
+            "-" => game.en_passant = None,
+            s => game.en_passant = match Game::position_to_bit(s) {
+                Err(e) => panic!("{}", e),
+                Ok(bit) => Some(bit),
+            },
+        }
+        // do halfmove clock
+        let rest = rest.to_string();
+        let (half, rest) = Game::split_on(&rest, ' ');
+        match half.parse() {
+            Ok(number) => game.half_move = number,
+            Err(_) => panic!("not a number for half move"),
+        }
+
+        let rest = rest.to_string();
+        let (full, _) = Game::split_on(&rest, ' ');
+        match full.parse() {
+            Ok(number) => game.full_move = number,
+            Err(_) => panic!("not a number for half move"),
+        }
         game
+    }
+
+    fn split_on(s: &String, split: char) -> (&str, &str) {
+        for (i, c) in s.chars().enumerate() {
+            if c == split {
+                return (&s[0..i], &s[i+1..])
+            }
+        }
+        (&s[..], "")
     }
 
     pub fn to_string(&self) -> String {
@@ -99,19 +161,41 @@ impl Game {
         }
         board
     }
-    // fn to_fn_string(&self) -> String {
-    //     let mut board = "".to_owned();
-    //     let mut temp = "".to_owned();
-    //     let mut space_counter = 0;
-    //     for(i, square) in self.squares.iter().enumerate() {
-    //         if i % 8 {
-    //             space_counter = 0;
-    //         }
-    //         match square {
-    //             Empty => 
-    //         }
-    //     }
-    // }
+
+    pub fn to_fen(&self) -> String {
+        // this function will take the game and return a FEN string
+        todo!()
+    }
+
+    pub fn position_to_bit(pos: &str) -> Result<PiecePosition, String>
+    {
+        if pos.len() != 2 {
+            return Err(format!("invalid len for enpass"))
+        } else {
+            let bytes = pos.as_bytes();
+            let byte0 = bytes[0];
+            if byte0 < 97 || byte0 >=97 + 8 {
+                return Err(format!("invalid position in pos to byte"))
+            } else {
+                let column = (byte0 - 97) as u32;
+                let byte1 = bytes[1];
+                let row;
+                match (byte1 as char).to_digit(10) {
+                    Some(number) => if number < 1 || number > 8 {
+                        return Err(format!("invalid position in byte: row"))
+                    } else {
+                        row = number - 1;
+                    },
+                    None => return Err(format!("invalid row char")),
+                }
+                let square_num = row * 8 + column;
+                println!("yerp");
+                println!("{}", square_num);
+                let bit = (1 as usize) << square_num;
+                Ok(bit)
+            }
+        }
+    }
 
     pub fn bit_to_position(&self, bit: PiecePosition) -> Result<String, String> {
         if bit == 0 {
--- Chess/src/main.rs
+++ Chess/src/main.rs
@@ -8,5 +8,8 @@ pub mod pieces;
 
 fn main() {
     let new_game = Game::initialize();
-    println!("{}", new_game.to_string())
+    println!("{}", new_game.to_string());
+    let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1".to_string();
+    let other = Game::fen_initialize(fen);
+    println!("{}", other.to_string());
 }
