4c3a9f3617f18d346b942a75cf5bf1ae17f49ff7
zlyons70 <zlyons70@gmail.com>
2025-02-02 02:03:14 -0500

    Started rayattacks

--- Chess/src/main.rs
+++ Chess/src/main.rs
@@ -2,14 +2,16 @@ use constants::{COL_MAP, MODTABLE, PiecePosition};
 mod constants;
 use game::Game;
 mod game;
+mod rayattacks;
 pub mod board;
 pub mod pieces;
 
-
 fn main() {
+    // instead of bevy use https://macroquad.rs/examples/
     let new_game = Game::initialize();
     println!("{}", new_game.to_string());
     let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1".to_string();
     let other = Game::fen_initialize(fen);
     println!("{}", other.to_string());
+    // now that I have the initial game state set up I need to use bevy to actually display the board
 }
---
+++ Chess/src/rayattacks.rs
@@ -0,0 +1,14 @@
+// we can create a struct to generate each type of move since pieces share similar moves
+
+pub struct Rays {
+    // each ray holds a vector of bit boards
+    n_rays: Vec<usize>,
+
+}
+
+impl Rays {
+    fn initialize() -> Self {
+        let n_rays = vec!();
+        
+    }
+}
